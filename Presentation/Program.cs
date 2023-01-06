470a04eac7f34f9f71ad63e40499539d450e553b
Danylo Krokhmaliuk <catanddogfromua@gmail.com>
2023-01-06 17:26:48 +0200

    Add auto-generated controllers and views for Course, Group and Student models

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Presentation
 {
     public class Program
@@ -8,6 +10,8 @@ namespace Presentation
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<Infrastructure.CoursesDbContext>(options =>
+                options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
 
             var app = builder.Build();
 
