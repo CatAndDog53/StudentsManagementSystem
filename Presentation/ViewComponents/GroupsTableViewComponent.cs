40f7c07a32cd9184a0fc647dfb54accb9d845b1e
Danylo Krokhmaliuk <catanddogfromua@gmail.com>
2023-01-12 14:42:31 +0200

    Edit Views, add GroupsTable and StudentsTable as separate ViewComponents

---
+++ Presentation/ViewComponents/GroupsTableViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model;
+using Services;
+
+namespace Presentation.ViewComponents
+{
+    public class GroupsTableViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GroupsTableViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int? courseId = null)
+        {
+            IEnumerable<Group> groups;
+
+            if (courseId.HasValue)
+            {
+                groups = await _unitOfWork.GroupsRepository.GetGroupsByCourseIdAsync(courseId.Value);
+            }
+            else
+            {
+                groups = await _unitOfWork.GroupsRepository.GetAllAsync();
+            }  
+
+            return View(groups);
+        }
+    }
+}
---
+++ Presentation/ViewComponents/StudentsTableViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model;
+using Services;
+
+namespace Presentation.ViewComponents
+{
+    public class StudentsTableViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public StudentsTableViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int? groupId = null)
+        {
+            IEnumerable<Student> students;
+
+            if (groupId.HasValue)
+            {
+                students = await _unitOfWork.StudentsRepository.GetStudentsByGroupIdAsync(groupId.Value);
+            }
+            else
+            {
+                students = await _unitOfWork.StudentsRepository.GetAllAsync();
+            }
+
+            return View(students);
+        }
+    }
+}
